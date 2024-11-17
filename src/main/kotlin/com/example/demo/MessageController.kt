8686ea28afc0a5058e5704ec41bb49072395c6a9
pdemeu <pdemeu.pessoal@gmail.com>
2024-11-17 15:02:30 -0300

    Feat:  Add a data class to the SpringBoot project

--- src/main/kotlin/com/example/demo/MessageController.kt
+++ src/main/kotlin/com/example/demo/MessageController.kt
@@ -7,5 +7,10 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class MessageController {
     @GetMapping("/")
-    fun index(@RequestParam("name") name: String) = "Hello, $name!"
+    fun listMessages() = listOf(
+        Message("1", "Hello!"),
+        Message("2", "Bonjour!"),
+        Message("3", "Bom dia!"),
+        Message("4", "Privet!")
+    )
 }
